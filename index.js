beab21f64187ee2d695f3b84c735e9548da90bc2
Luca Castelnuovo <ltcastelnuovo@gmail.com>
2020-04-17 15:10:21 +0200

    Add datatype check

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 module.exports = formData => {
+    if (!(formData instanceof FormData)) throw new TypeError('Expected FormData, got ' + (typeof formData))
+
     const object = {};
     
     [...formData].map(item => object[item[0]]=item[1]);
